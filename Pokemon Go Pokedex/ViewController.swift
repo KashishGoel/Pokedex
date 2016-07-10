960da208606b57a802805312d4a9df9172190b83
Kashish Goel <kashish98@hotmail.com>
2016-07-10 17:29:33 -0400

    Initial Commit

---
+++ Pokemon Go Pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon Go Pokedex
+//
+//  Created by Kashish Goel on 2016-07-10.
+//  Copyright © 2016 Kashish Goel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
