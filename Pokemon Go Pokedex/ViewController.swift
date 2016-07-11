1af496c070ee4475d6308b53a5466432ea4f6b98
Kashish Goel <kashish98@hotmail.com>
2016-07-11 01:13:49 -0400

    tableView delegate/datasource setup

--- Pokemon Go Pokedex/ViewController.swift
+++ Pokemon Go Pokedex/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView:UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PokemonCell") as? PokemonCell {
+        return cell
+        }
+        
+        else {
+            
+        return tableView.dequeueReusableCellWithIdentifier("PokemonCell") as! PokemonCell!
+        }
+        
+    }
 }
+
 
