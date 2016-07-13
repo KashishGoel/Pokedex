1b2918c7b7be26df87afbab5c91ded7aac8d3d0b
Kashish Goel <kashish98@hotmail.com>
2016-07-13 15:37:05 -0400

    pokemon ttype label background colours reflect pokemon's type

--- Pokemon Go Pokedex/Extensions.swift
+++ Pokemon Go Pokedex/Extensions.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension Array{
     
@@ -30,4 +31,20 @@ extension Array{
         
         
     }
+}
+
+extension UIColor {
+    
+    convenience init(hex: Int) {
+        
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
+        
+    }
+    
 }
--- PokemonCell.swift
+++ PokemonCell.swift
@@ -5,7 +5,7 @@
 //  Created by Kashish Goel on 2016-07-11.
 //  Copyright © 2016 Kashish Goel. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
 class PokemonCell: UITableViewCell {
@@ -17,6 +17,9 @@ class PokemonCell: UITableViewCell {
  
     
     var pokemon: Pokemon!
+   // var color:Int!
+    
+    let typeColor = ["Water":0x4993C2, "Grass":0x9CCA58, "Poison":0xB881C7, "Fire":0xFB7D33, "Bug":0x739E45,"Normal":0xA4ACAF, "Electric":0xEDD447, "Ground":0xAA9748, "Fairy":0xFCBAE8, "Fighting":0xD3672E, "Psychic":0xF169B8, "Flying":0x46C7ED, "Rock":0xA28B2,  ]
     
     
     
@@ -27,9 +30,28 @@ class PokemonCell: UITableViewCell {
         img.image = UIImage(named: "\(self.pokemon.id)")
         if self.pokemon.type2 != "" {
         type2.text = pokemon.type2
-        type1.text = pokemon.type1}
+        type1.text = pokemon.type1
+            if let color1:Int = typeColor[pokemon.type1],color2:Int = typeColor[pokemon.type2] {
+               
+                type1.backgroundColor = UIColor(hex: color1)
+                type2.backgroundColor = UIColor(hex: color2)
+                
+            }
+            
+        
+        }
+            
         
         else{
+            soloType.hidden = false
+            soloType.text = pokemon.type1
+            
+            if let color:Int = typeColor[pokemon.type1]{
+            soloType.backgroundColor = UIColor(hex: color)
+            }
+            
+            type2.hidden = true
+            type1.hidden = true
         
         }
         
